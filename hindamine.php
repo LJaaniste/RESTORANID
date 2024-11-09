98e0f6b792900653561dd04d24f08bd1f8cc1eaf
LJaaniste <leenajaaniste@gmail.com>
2024-11-09 20:02:10 +0200

    lj

--- hindamine.php
+++ hindamine.php
@@ -116,26 +116,10 @@
             <div class="col-sm-4">Hinnang:</div>
             <div class="col-sm-8">
                 <div class="rate">
-                <input type="radio" id="star10" name="rate" value="10" required/>
-                    <label for="star10" title="text">10 stars</label>
-                    <input type="radio" id="star9" name="rate" value="9" />
-                    <label for="star9" title="text">9 stars</label>
-                    <input type="radio" id="star8" name="rate" value="8" />
-                    <label for="star8" title="text">8 stars</label>
-                    <input type="radio" id="star7" name="rate" value="7" />
-                    <label for="star7" title="text">7 stars</label>
-                    <input type="radio" id="star6" name="rate" value="6" />
-                    <label for="star6" title="text">6 star</label>
-                    <input type="radio" id="star5" name="rate" value="5" />
-                    <label for="star5" title="text">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4" />
-                    <label for="star4" title="text">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3" />
-                    <label for="star3" title="text">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="text">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="text">1 star</label>
+                <?php for ($i = 10; $i >= 1; $i--) : ?>
+                        <input type="radio" id="star<?php echo $i; ?>" name="rate" value="<?php echo $i; ?>" required/>
+                        <label for="star<?php echo $i; ?>"><?php echo $i; ?> stars</label>
+                    <?php endfor; ?>
                 </div>
             </div>
         </div>
